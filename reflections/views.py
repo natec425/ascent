9f0ff6c4d784d5b030ed22b6fcdb1bc6ddb16df4
Patton Burge <pburge@basecampcodingacademy.org>
2019-12-06 15:54:26 -0600

    Small changes working toward ticket #3

--- reflections/views.py
+++ reflections/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from .models import Reflection,Submission,Question
+from .models import Reflection, Submission, Question
+from django.utils import timezone
+from datetime import datetime
+
 
 def home(request):
     reflections = Reflection.objects.all()
-    questions = Question.objects.all()
-    return render(request, "reflections/base.html", {"reflections": reflections, "questions":questions})
+    for reflection in reflections:
+        if reflection.date == timezone.now().date():
+            questions = Question.objects.filter
+            return render(request, "reflections/base.html", {"reflection": reflection,"questions": questions})
+        else:
+            return render(request, "reflections/base.html", {"reflection": reflection})
 
-def submit_reflection(request,submit_id):
-    reflection = Reflection.objects.get(id=submit_id)
-    Submission.objects.create(
-        reflection=reflection , user=request.user
-    )
-    return render(request, "reflections/base.html")
+
+
+def submit_reflection(request, submit_id):
+    pass
