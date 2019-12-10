feafbec0955d28b42c71e06f4f46e0ea6e4fbfdd
Patton Burge <pburge@basecampcodingacademy.org>
2019-12-10 08:12:19 -0600

    configured url's, models, and views

--- reflections/views.py
+++ reflections/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Reflection, Submission, Question
-from django.utils import timezone
+from django.utils import timezone,dateformat
 from datetime import datetime
 
 
 def home(request):
     reflections = Reflection.objects.all()
     for reflection in reflections:
-        if reflection.date == timezone.now().date():
-            questions = Question.objects.filter
+        if reflection.date == datetime.utcnow().date():
+            questions = Question.objects.filter(reflection=reflection)
             return render(request, "reflections/base.html", {"reflection": reflection,"questions": questions})
         else:
             return render(request, "reflections/base.html", {"reflection": reflection})
 
-
-
-def submit_reflection(request, submit_id):
-    pass
+def submit_reflection(request,id):
+    question = Question.objects.get(id=id)
+    return render(request, "reflections/base.html",{"question": question})
